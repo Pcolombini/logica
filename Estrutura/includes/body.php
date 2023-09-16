9bc9826430fbe807fbf6d55b368295b6d8c1bf2d
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-09-16 19:46:11 -0300

    Estrura de arquivos repetidos

---
+++ Estrutura/includes/body.php
@@ -0,0 +1,24 @@
+<body>
+    <header class="title">
+        <h1></h1>
+    </header>
+
+    <article>
+        <section class="ex">
+            <p class="ex">
+
+            </p>
+        </section>
+        
+    </article>
+    <main>
+        <section class="resultado">
+            <p>
+                <?php include 'exercicio.php' ?>
+            </p>
+        </section>
+    </main>
+
+</body>
+
+</html>
