b63fee3c3f117d1986df280450c8c07a2e5582ea
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-09-20 20:39:39 -0300

    add files incompletos

---
+++ Modulo 2/005/includes/body.php
@@ -0,0 +1,48 @@
+<body>
+    <header class="title">
+        <h1>Equação de 2 grau</h1>
+    </header>
+
+    <article>
+        <section class="ex">
+            <p class="ex">
+            <p>
+                Exercício de lógica de programação - bhaskara.
+            </p>
+            <hr>
+            <p>
+            <h3>
+                Exercício de lógica de programação
+            </h3>
+            </p>
+            <p>
+                Utilizando funções, faça um programa que calcule as raízes da equação do 2 grau conforme a fórmula de Bhaskara.
+            </p>
+            <p>
+                Lembrando:
+            </p>
+            <p>
+            <pre class="code">
+                <code>
+                        ax<sup>2</sup> + bx + c = 0 
+
+                        delta = (Δ = b<sup>2</sup> - 4.a.c)
+                        x<sup>1</sup>    = ( (-b + √Δ)/2a)
+                        x<sup>2</sup>    = ( (-b - √Δ)/2a)
+                </code>
+            </pre>
+            </p>
+            </p>
+        </section>
+
+    </article>
+    <main>
+        <?php include 'exercicio.php' ?>
+        <section class="resultado">
+            <p>
+                <?php
+
+                ?>
+            </p>
+        </section>
+    </main>
